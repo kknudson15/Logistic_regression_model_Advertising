268c329e87898c1a2f444877d424bdada4a7c8f6
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-08-05 12:29:51 -0500

    modularized and made data exploration optional.

--- Logistic_regression_project.py
+++ Logistic_regression_project.py
@@ -3,74 +3,65 @@ Logistic Regression Portfolio Project
 advertising Data
 '''
 
-import pandas as pd 
-import numpy as np 
+import pandas as pd
 import seaborn as sns 
-import matplotlib.pyplot as plt 
-
-
-ad_data = pd.read_csv('advertising.csv')
-#print(ad_data.head())
-#print(ad_data.info())
-#print(ad_data.describe())
-
-'''
-Exploratory Data Analysis
-Create a histogram of the Age
-Create a joinplot showing Area income versus Age
-Create a Joinplot sjowing the kde distributions of Daily time spent on site vs. Age
-Create a joinplot of Daily Time Spent on Site vs. Daily Internet Usage
-Create a pairplot with the hue defined by the 'Clicked on Ad' column Feature
-'''
-plt.hist(ad_data['Age'], bins = 50)
-plt.show()
-
-sns.jointplot(ad_data['Age'], ad_data['Area Income'], data = ad_data)
-plt.show()
-
-sns.jointplot(ad_data['Age'], ad_data['Daily Time Spent on Site'], data = ad_data, kind = 'kde')
-plt.show()
-
-sns.jointplot(ad_data['Daily Time Spent on Site'], ad_data['Daily Internet Usage'], data = ad_data)
-plt.show()
-
-sns.pairplot(ad_data, hue = 'Clicked on Ad', diag_kind= 'scatter')
-plt.show()
-
-
-'''
-Logistic Regression
-Split the data into training set and testing set using train_test_split
-Train and fit a Logistic Regression Model on the training set
-'''
-
-X = ad_data[['Age', 'Area Income', 'Daily Time Spent on Site','Daily Internet Usage', 'Male']]
-y = ad_data['Clicked on Ad']
-
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
-
 from sklearn.linear_model import LogisticRegression
-
-logmodel = LogisticRegression()
-logmodel.fit(X_train, y_train)
-
-
-'''
-Predictions and Evaluations
-Predict values for the testing data
-Create a classification report for the model
-'''
-predictions = logmodel.predict(X_test)
-
-
 from sklearn.metrics import classification_report
-print(classification_report(y_test, predictions))
-
 from sklearn.metrics import confusion_matrix
-print(confusion_matrix(y_test, predictions))
+
+def gather_data(file):
+    ad_data = pd.read_csv(file)
+    print(ad_data.head())
+    return ad_data
+
+def explore_data(data_frame):
+    print(data_frame.info())
+    print(data_frame.describe())
+
+    plt.hist(data_frame['Age'], bins = 50)
+    plt.show()
+
+    sns.jointplot(data_frame['Age'], data_frame['Area Income'], data = data_frame)
+    plt.show()
+
+    sns.jointplot(data_frame['Age'], data_frame['Daily Time Spent on Site'], data =data_frame, kind = 'kde')
+    plt.show()
+
+    sns.jointplot(data_frame['Daily Time Spent on Site'], data_frame['Daily Internet Usage'], data = data_frame)
+    plt.show()
+
+    sns.pairplot(data_frame, hue = 'Clicked on Ad', diag_kind= 'scatter')
+    plt.show()
 
 
+def split_data(data_frame):
+    X = data_frame[['Age', 'Area Income', 'Daily Time Spent on Site','Daily Internet Usage', 'Male']]
+    y = data_frame['Clicked on Ad']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+    return X_train, X_test, y_train, y_test
 
+def train_model(X_train, y_train):
+    logmodel = LogisticRegression()
+    logmodel.fit(X_train, y_train)
+    return logmodel
 
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    print(classification_report(y_test, predictions))
+    print(confusion_matrix(y_test, predictions))
 
+if __name__ == '__main__':
+    filename = 'advertising.csv'
+    ad_data = gather_data(filename)
+    explore = input('Do you want to explore the data further?')
+    if explore == 'yes':
+        explore_data(ad_data)
+    data = split_data(ad_data)
+    X_train = data[0]
+    X_test = data[1]
+    y_train = data[2]
+    y_test = data[3]
+    model = train_model(X_train, y_train)
+    evaluate_model(model, X_test, y_test)
